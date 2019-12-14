f087bc467a89a6d81e1aaa52c2ece3820f2b6f46
Jeff Glanville <jeff.glanville@outlook.com>
2019-12-14 11:36:20 -0600

    update head js file with elements, classes and text content

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,17 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const h1Header = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    date.textContent = 'March 28, 2019';
+    h1Header.textContent = 'Lambda Times';
+    temp.textContent = '98' + '&deg';
+}
